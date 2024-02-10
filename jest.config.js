7030ef7dbaf9bf71d70cbb4f9540ce4b12bc2d95
Matthew Ball <mball@quartermaster.house>
2024-02-10 14:23:19 -0500

    Fix failing test suite configuration

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
 };
